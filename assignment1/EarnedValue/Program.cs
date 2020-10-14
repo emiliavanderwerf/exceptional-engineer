02c8e1b853e1a332566bc1324cb29017c4051480
Emilia Vanderwerf <evanderwerf@catchpoint.com>
2020-10-13 21:42:26 -0400

    Add assignment 1 starter; start implementation for inputting schedule plan and task plan data

---
+++ assignment1/EarnedValue/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Xunit;
+
+namespace EarnedValue
+{
+    public class Program
+    {
+        [Theory]
+        [InlineData("SchedulePlan_ExampleData.json", "TaskPlan_ExampleData.json")]
+        [InlineData("SchedulePlan_100Tasks.json", "TaskPlan_100Tasks.json")]
+        public void Should_Calculate_Earned_Value_Plan(string schedulePlanPath, string taskPlanPath)
+        {
+
+        }
+
+        [Theory]
+        [InlineData("SchedulePlan_IllegalData.json", "TaskPlan_IllegalData.json")]
+        public void Should_Throw_Exception_For_Illegal_Data(string schedulePlanPath, string taskPlanPath)
+        {
+
+        }
+    }
+}
