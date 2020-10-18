20daebabd743864327bad286a4401d6ac830efb0
Emilia Vanderwerf <emilia.vanderwerf@gmail.com>
2020-10-18 14:16:13 -0400

    Create initial Visual Studio C# Class Library for assignment 2 - Correlation

---
+++ assignment2/Correlation/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xunit;
+
+namespace Correlation
+{
+    public class Program
+    {
+        [Theory]
+        [InlineData(0)]
+        public void Test(int zero)
+        {
+
+        }
+    }
+}
