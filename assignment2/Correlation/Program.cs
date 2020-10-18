5978fcbce26eec2dad4ccad26671da06252f5fb1
Emilia Vanderwerf <emilia.vanderwerf@gmail.com>
2020-10-18 14:42:52 -0400

    Test data and unit test framework

--- assignment2/Correlation/Program.cs
+++ assignment2/Correlation/Program.cs
@@ -1,15 +1,56 @@
-﻿using System;
+﻿using System.Collections.Generic;
 using Xunit;
 
 namespace Correlation
 {
     public class Program
     {
-        [Theory]
-        [InlineData(0)]
-        public void Test(int zero)
+        [Fact]
+        public void Should_Calculate_Using_EstimatedProxySize_And_ActualAddedModifiedSize()
         {
+            List<int> estimatedProxySize = TestData.EstimatedProxySize;
+            List<int> actualAddedModifiedSize = TestData.ActualAddedModifiedSize;
+            double B0 = -22.55;
+            double B1 = 1.7279;
+            double r = 0.9545;
+            double rSquared = 0.9111;
+            double P = 644.429;
+        }
 
+        [Fact]
+        public void Should_Calculate_Using_EstimatedProxySize_And_ActualDevelopmentTime()
+        {
+            List<int> estimatedProxySize = TestData.EstimatedProxySize;
+            List<double> actualDevelopmentHours = TestData.ActualDevelopmentHours;
+            double B0 = -4.039;
+            double B1 = 0.1681;
+            double r = 0.9333;
+            double rSquared = 0.8711;
+            double P = 60.858;
+        }
+
+        [Fact]
+        public void Should_Calculate_Using_PlannedAddedModifiedSize_And_ActualAddedModifiedSize()
+        {
+            List<int> plannedAddedModifiedSize = TestData.PlannedAddedModifiedSize;
+            List<int> actualAddedModifiedSize = TestData.ActualAddedModifiedSize;
+            double B0 = 23.92;
+            double B1 = 1.43097;
+            double r = 0.9631;
+            double rSquared = 0.9276;
+            double P = 528.4294;       
+        }
+
+        [Fact]
+        public void Should_Calculate_Using_PlannedAddedModifiedSize_And_ActualDevelopmentTime()
+        {
+            List<int> plannedAddedModifiedSize = TestData.PlannedAddedModifiedSize;
+            List<double> actualDevelopmentHours = TestData.ActualDevelopmentHours;
+            double B0 = -4.604;
+            double B1 = 0.140164;
+            double r = 0.9480;
+            double rSquared = 0.8988;
+            double P = 49.4994;        
         }
     }
 }
---
+++ assignment2/Correlation/TestData.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Correlation
+{
+    public class TestData
+    {
+        public static readonly List<int> EstimatedProxySize = new List<int>
+        {
+            130,
+            650,
+            99,
+            150,
+            128,
+            302,
+            95,
+            945,
+            368,
+            961
+        };
+
+        public static readonly List<int> PlannedAddedModifiedSize = new List<int>
+        {
+            163,
+            765,
+            141,
+            166,
+            137,
+            355,
+            136,
+            1206,
+            433,
+            1130
+        };
+
+        public static readonly List<int> ActualAddedModifiedSize = new List<int>
+        {
+            186,
+            699,
+            132,
+            272,
+            291,
+            331,
+            199,
+            1890,
+            788,
+            1601
+        };
+
+        public static readonly List<double> ActualDevelopmentHours = new List<double>
+        {
+            15.0,
+            69.9,
+            6.5,
+            22.4,
+            28.4,
+            65.9,
+            19.4,
+            198.7,
+            38.8,
+            138.2
+        };
+    }
+}
